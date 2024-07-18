26052e9b38dc8c731b2bac919257102cdd4db950
Dinkles <isinetov@mail.ru>
2024-07-18 18:31:16 +0400

    Реализовал подключение бд и изъятие данных из таблицы NetworkUser с помощью отсоединенной модели.

---
+++ ADOtest/DbExecutor.cs
@@ -0,0 +1,28 @@
+﻿using ADOtest;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ADOtestModule
+{
+    public class DbExecutor
+    {
+        private MainConnector connector;
+
+        public DbExecutor(MainConnector connector)
+        {
+            this.connector = connector;
+        }
+
+        public DataTable SelectAll(string table)
+        {
+            var selectcommandtext = "select * from " + table;
+            var adapter = new SqlDataAdapter(
+            selectcommandtext,
+            connector.GetConnection()
+            );
+            var ds = new DataSet();
+            adapter.Fill(ds);
+            return ds.Tables[0];
+        }
+    }
+}
