525c4562497fa0cd8efed66fdc7f3de7504d3e9d
Dinkles <isinetov@mail.ru>
2024-07-18 21:29:45 +0400

    Реализовал подключение к бд и извлечение данных из таблицы NetworkUser с помощью присоединенной модели.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ADOtest;
 using ADOtestModule;
+using Microsoft.Data.SqlClient;
 using System.Data;
 class Program 
 {
@@ -20,24 +21,51 @@ class Program
 
             Console.WriteLine("Получаем данные таблицы " + tablename);
 
-            data = db.SelectAll(tablename);
+            //data = db.SelectAll(tablename);
 
-            Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
+            //Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
 
-            foreach (DataColumn column in data.Columns) 
+            //foreach (DataColumn column in data.Columns) 
+            //{
+            //    Console.Write($"{column.ColumnName}\t");
+            //}
+
+            //Console.WriteLine();
+
+            //foreach (DataRow row in data.Rows)
+            //{
+            //    var cells = row.ItemArray;
+            //    foreach (var cell in cells)
+            //    {
+            //        Console.Write($"{cell}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            SqlDataReader reader = db.SelectAllCommandReader(tablename);
+            
+            var columnList = new List<string>();
+
+            for (int i = 0; i < reader.FieldCount; i++)
             {
-                Console.Write($"{column.ColumnName}\t");
+                var name = reader.GetName(i);
+                columnList.Add(name);
             }
 
+            for (int i = 0; i < columnList.Count; i++)
+            {
+                Console.Write($"{columnList[i]}\t");
+            }
             Console.WriteLine();
 
-            foreach (DataRow row in data.Rows)
+            while (reader.Read())
             {
-                var cells = row.ItemArray;
-                foreach (var cell in cells)
+                for (int i = 0; i < columnList.Count; i++)
                 {
-                    Console.Write($"{cell}\t");
+                    var value = reader[columnList[i]];
+                    Console.Write($"{value}\t");
                 }
+
                 Console.WriteLine();
             }
 
