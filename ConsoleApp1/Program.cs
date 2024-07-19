9e5fd980e245ee6727135000d2c43eb5d549adea
Dinkles <isinetov@mail.ru>
2024-07-19 20:00:30 +0400

    Выполнил финальный рефакторинг. Реализовал пользовательский интерфейс с помощью ввода команд.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,46 +1,18 @@
-﻿using ADOtest;
-using ADOtestModule;
-using ConsoleModule;
-using Microsoft.Data.SqlClient;
-using System.Data;
+﻿using ConsoleModule;
+public enum Commands
+{
+    stop,
+    add,
+    delete,
+    update,
+    show
+}
+
 class Program 
 {
-    static void Main(string[] args)
+    static Manager manager = new Manager();
+    static void Add() 
     {
-        //data = db.SelectAll(tablename);
-
-        //Console.WriteLine("Количество строк в " + tablename + ": " + data.Rows.Count);
-
-        //foreach (DataColumn column in data.Columns) 
-        //{
-        //    Console.Write($"{column.ColumnName}\t");
-        //}
-
-        //Console.WriteLine();
-
-        //foreach (DataRow row in data.Rows)
-        //{
-        //    var cells = row.ItemArray;
-        //    foreach (var cell in cells)
-        //    {
-        //        Console.Write($"{cell}\t");
-        //    }
-        //    Console.WriteLine();
-        //}
-
-        Manager manager = new Manager();
-
-        manager.Connect();
-
-        manager.ShowData();
-
-        Console.Write("Введите логин для удаления: ");
-        var countDeleted = manager.DeleteUserByLogin(Console.ReadLine());
-
-        Console.WriteLine($"Строк удалено: {countDeleted}");
-
-        manager.ShowData();
-
         Console.Write("Введите имя нового пользователя: ");
         var newUserName = Console.ReadLine();
         Console.Write("Введите логин нового пользователя: ");
@@ -48,9 +20,22 @@ class Program
         var countAdded = manager.AddNewUser(newUserName, newUserLogin);
 
         Console.WriteLine($"Строк добавлено: {countAdded}");
-        
-        manager.ShowData();
 
+        manager.ShowData();
+    }
+
+    static void Delete() 
+    {
+        Console.Write("Введите логин для удаления: ");
+        var countDeleted = manager.DeleteUserByLogin(Console.ReadLine());
+
+        Console.WriteLine($"Строк удалено: {countDeleted}");
+
+        manager.ShowData();
+    }
+
+    static void Update() 
+    {
         Console.Write("Введите логин пользователя, имя которого вы хотите изменить: ");
         var userLogin = Console.ReadLine();
         Console.Write("Введите новое имя пользователя: ");
@@ -60,6 +45,50 @@ class Program
         Console.WriteLine($"Строк изменено: {countUpdated}");
 
         manager.ShowData();
+    }
+
+    static void Main(string[] args)
+    {
+        string command;
+
+        manager.Connect();
+
+        Console.WriteLine("Список команд для работы консоли:");
+        Console.WriteLine(Commands.stop + ": прекращение работы");
+        Console.WriteLine(Commands.add + ": добавление данных");
+        Console.WriteLine(Commands.delete + ": удаление данных");
+        Console.WriteLine(Commands.update + ": обновление данных");
+        Console.WriteLine(Commands.show + ": просмотр данных");
+
+        do
+        {
+            Console.Write("Введите команду: ");
+            command = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (command)
+            {
+                case nameof(Commands.show):
+                    manager.ShowData();
+                    break;
+                case nameof(Commands.add):
+                    Add();
+                    break;
+                case nameof(Commands.delete): 
+                    Delete();
+                    break;
+                case nameof(Commands.update): 
+                    Update(); 
+                    break;
+                case nameof(Commands.stop):
+                    Console.WriteLine("Выход из программы...");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели неверную команду...");
+                    break;
+            }
+        }
+        while (command != nameof(Commands.stop));
 
         manager.Disconnect();
 
